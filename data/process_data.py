2a2fcb7d00f427b49be451652d8071d0a49a1498
bdamadi <bdamadi@gmail.com>
2023-01-08 10:12:29 -0500

    Finish process_data script

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,60 @@
 import sys
-
+import pandas as pd
+from sqlalchemy.engine import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    
+    # merge datasets
+    df = messages.merge(categories, on='id')
 
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
 
-def clean_data(df):
-    pass
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x: x[:-2])
 
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # Convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1:]
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df.drop(columns=['categories'], inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    return pd.concat([df, categories], axis=1)
+
+def clean_data(df):    
+    # Drop NA messages
+    num_rows = df.shape[0]
+    df = df.dropna(subset=['message'])
+    print('    Dropped missing data : {}'.format(num_rows - df.shape[0]))
+
+    # drop duplicates
+    num_rows = df.shape[0]
+    df = df.drop_duplicates()
+    print('    Removed duplicated data : {}'.format(num_rows - df.shape[0]))
+
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    # Save the clean dataset into an sqlite database
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('DisasterResponse', engine, index=False, if_exists='replace')
 
 
 def main():
