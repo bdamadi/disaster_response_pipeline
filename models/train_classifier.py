e0679542485c64e1d96bc746a01ee877dbb9f2d6
bdamadi <bdamadi@gmail.com>
2023-02-10 01:59:18 -0500

    Classifier with RandomForest

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,63 @@
 import sys
+import pandas as pd
+import numpy as np
+import pickle
 
+from sqlalchemy import create_engine
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+
+# import nltk
+# nltk.download('punkt')
+# nltk.download('wordnet')
 
 def load_data(database_filepath):
-    pass
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DisasterResponse', engine)
 
+    X = df["message"]
+    Y = df.drop(["id", "message", "original", "genre"], axis=1)
+    return X, Y, Y.columns.values
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('count', CountVectorizer(tokenizer=tokenize)),
+        ('tf-idf', TfidfTransformer()),
+        ('classifier', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+    # Use np.hstack
+    # https://stackoverflow.com/questions/56826216/valueerror-unknown-label-type-for-classification-report
+    print(classification_report(np.hstack(Y_pred), np.hstack(Y_test.values)))
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
